225fa29f91019252b11f409ff260a8b5a7f2fca7
Francisco Ruiz <fdosruiz@gmail.com>
2024-07-27 13:33:43 +0200

    test: add Middleware unit tests and update test scripts for lib code testing
    Introduced unit tests for Middleware using common tests setup. Mocked Middleware in Container tests instead of Cache. Enhanced test scripts for better watch capabilities and added .prettierrc to .npmignore. Updated node engine requirement in package-lock.json.

--- test/lib/core/Container.test.js
+++ test/lib/core/Container.test.js
@@ -1,16 +1,22 @@
 import Container from '../../../lib/core/Container';
-import Cache from '../../../lib/core/Cache';
+import Middleware from '../../../lib/core/Middleware';
 import { containerCommonTests } from '../../common/Container.commonTests';
 
-jest.mock('../../../lib/core/Cache', () => {
-    const memorizeMethodsMock = jest.fn();
-    return jest.fn().mockImplementation(() => ({
-        memorizeMethods: memorizeMethodsMock,
-        _getMemorizeMethodsMock: () => memorizeMethodsMock, // provide a getter to access the mock
-    }));
+jest.mock('../../../lib/core/Middleware', () => {
+  const middlewareGetProxyMock = jest.fn();
+  return jest.fn().mockImplementation(() => {
+    return {
+      getProxy: middlewareGetProxyMock,
+      _getMemorizeMethodsMock: () => middlewareGetProxyMock, // provide a getter to access the mock
+    };
+  });
 });
 
-describe('Testing Container LIB', () => {
-  const cache = new Cache();
-  containerCommonTests(Container, cache._getMemorizeMethodsMock());
+describe('Unit Testing for Container (Lib code)', () => {
+  const middleware = new Middleware();
+  const mocks = {
+    middlewareGetProxyMock: middleware._getMemorizeMethodsMock(),
+  };
+
+  containerCommonTests(Container, mocks);
 });
--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -1,7 +1,7 @@
 import { Container } from "../../lib/core";
 import container from '../../lib';
 
-describe('testing entry point', () => {
+describe('Unit Testing for entry point (Lib code)', () => {
   it('should get the instance of the container', () => {
     expect(container).toBeInstanceOf(Container);
     expect(typeof container).toBe('object');
