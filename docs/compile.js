d16fbf0afb7dfe55c87e79fae1f47b98fa949cd5
Francisco Ruiz <fdosruiz@gmail.com>
2024-08-29 23:07:55 +0200

    docs: add custom compilation script for markdown docs [PTJSD-20]
    Introduced a new compile.js script to handle markdown document preprocessing with middleware interception for processFile and writeFile methods. Updated file paths and configuration structure to accommodate the new compilation process. Modified package.json build scripts to utilize the custom compilation script.

---
+++ docs/compile.js
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const config = require('./config');
+const { default: packetJsDi } = require('../lib/index.js');
+
+// Add markdown-include to container
+const key = 'markdown-include';
+packetJsDi.add(key, () => require('markdown-include'));
+
+// Add middleware to intercept processFile
+packetJsDi.middleware.add(key, (next, context, args) => {
+  const [file] = args;
+  const result = next(args);
+
+  if (context.methodName === 'processFile') {
+    const build = context.container.get(key).build;
+    switch (file) {
+      case config.processTitles.npmjsReadme:
+        build[file].parsedData = build[file].parsedData.replace(/^## /gm, '### '); // h2 to h3
+        build[file].parsedData = build[file].parsedData.replace(/\n# /, '\n## '); // second h1 to h2
+        break;
+      case config.processTitles.wikiHome:
+        build[file].parsedData = build[file].parsedData.replace(/^## /gm, '### '); // h2 to h3
+        break;
+      default:
+        break;
+    }
+  }
+
+  if (context.methodName === 'writeFile') {
+    console.log(`
+      - ${context.container.get(key).options.build}
+        have been built successfully from entry point:
+        > ${context.container.get(key).options.files}
+    `);
+  }
+  return result;
+});
+
+const markdownInclude = packetJsDi.get(key);
+
+const originalProcessFile = markdownInclude.processFile;
+const originalWriteFile = markdownInclude.writeFile;
+
+// Override functions to allow middleware interception
+markdownInclude.processFile = (path) => originalProcessFile.call(this, path);
+markdownInclude.writeFile = (path) => originalWriteFile.call(this, path);
+
+config.compileFiles.forEach((file) => {
+  markdownInclude.compileFiles(`${config.compileBasePath}${file}`);
+});
